be2477b25c7f72eecd7c61a88f56bca05505bfde
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-02-17 23:58:42 -0800

    nothing much

---
+++ generator.js
@@ -0,0 +1,60 @@
+// const cards = new Array();
+// $.get("Salamangreats.txt", function(data) {
+//   cards = data.split(",");
+//   console.log(cards);
+// });
+
+const cards = [
+  "Salamangreat Gazelle",
+  "Salamangreat Gazelle",
+  "Salamangreat Gazelle",
+  "Salamangreat Spinny",
+  "Salamangreat Spinny",
+  "Salamangreat Spinny",
+  "Lady Debug",
+  "Lady Debug",
+  "Lady Debug",
+  "Salamangreat Foxy",
+  "Salamangreat Foxy",
+  "Salamangreat Jack Jaguar",
+  "Salamangreat Falco",
+  "Ash Blossom & Joyous Spring",
+  "Ash Blossom & Joyous Spring",
+  "Ash Blossom & Joyous Spring",
+  "Effect Veiler",
+  "Effect Veiler",
+  "Backup Secretary",
+  "Salamangreat Circle",
+  "Salamangreat Circle",
+  "Salamangreat Circle",
+  "Fusion of Fire",
+  "Will of the Salamangreat",
+  "Salamangreat Sanctuary",
+  "Salamangreat Sanctuary",
+  "Fusion of Fire",
+  "Called by the Grave",
+  "Called by the Grave",
+  "Called by the Grave",
+  "Twin Twisters",
+  "Twin Twisters",
+  "Foolish Burial",
+  "Upstart Goblin",
+  "Super Polymerization",
+  "Salamangreat Roar",
+  "Salamangreat Roar",
+  "Salamangreat Rage",
+  "Gozen Match",
+  "Gozen Match",
+  "Mirror Force"
+];
+
+let openingHand = []
+
+for (let i = 0; i < 5; i++) {
+  let draw = cards[Math.floor(Math.random() * cards.length)];
+  openingHand.push(" " + draw)
+}
+
+console.log(openingHand);
+$(".openingHand").text(openingHand)
+
